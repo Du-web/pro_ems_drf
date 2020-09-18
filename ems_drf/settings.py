7b98775bf213c0f8897748c31d7c497cc412cc4b
dudu <648302938@qq.com>
2020-09-18 20:37:10 +0800

    配置CORS跨域

--- ems_drf/settings.py
+++ ems_drf/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'emsapp',
 ]
@@ -45,7 +46,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -131,3 +133,5 @@ REST_FRAMEWORK = {
     # 默认的全局异常的处理方法
     'EXCEPTION_HANDLER': 'utils.exceptions.exception_handler',
 }
+
+CORS_ORIGIN_ALLOW_ALL= True
